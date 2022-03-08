0b81e95f40c51f81930528e68314b33c9ef19b2e
Nuno Teixeira <1210869@isep.ipp.pt>
2022-03-08 12:13:45 +0000

    saving

--- TP1/MainAutomovel.java
+++ TP1/MainAutomovel.java
@@ -1,6 +1,53 @@
 public class MainAutomovel {
     public static void main(String[] args) {
 
+        // a)
+        Automovel a1 = new Automovel("11-11-AA", "Toyota", 1400);
+
+        // b)
+        System.out.println(a1.toString());
+
+        // c)
+        System.out.println(a1.getMatricula());
+
+        // d)
+        System.out.println(Automovel.getTotalAutomoveis());
+
+        // e)
+        Automovel a2 = new Automovel("22-22-BB", "Audi");
+
+        // f)
+        System.out.println(a2.toString());
+
+        // g)
+        a2.setCilindrada(1800);
+
+        // h)
+        System.out.println(a2.toString());
+
+        // i)
+        System.out.println(Automovel.getTotalAutomoveis());
+
+        // j)
+        int dif  = a1.getCilindrada() - a2.getCilindrada();
+        System.out.println(Math.abs(dif));
+
+        // k)
+        if (dif > 0) {
+            System.out.println(a1.getMatricula());
+        } else {
+            System.out.println(a2.getMatricula());
+        }
+
+        // l)
+        if (a1.getCilindrada() > 2000) {
+            System.out.println("é maior que 2000");
+        } else {
+            System.out.println("não é maior que 2000");
+        }
+
+
+
 
 
 
